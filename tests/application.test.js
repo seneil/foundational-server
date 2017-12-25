7d2877d2bf2ba753a1798075ff0dbf43dd3bf2fe
dshster <dmitry@shvalyov.ru>
2017-12-25 20:33:43 +0300

    [KPR-35] Test development.

---
+++ tests/application.test.js
@@ -0,0 +1,279 @@
+const mongoose = require('mongoose');
+const nock = require('nock');
+const request = require('supertest');
+const mockDocument = require('./mock.document');
+const mockDummyDocument = require('./mock.dummy.document');
+
+const { OK, NOT_FOUND } = require('../application/router/constants/answer-codes');
+
+const db = mongoose.connection;
+const application = require('../application');
+
+describe('Запросы к серверу', () => {
+  beforeEach(done => {
+    nock('http://example.com')
+      .defaultReplyHeaders({
+        'Content-Type': 'text/html',
+      })
+      .get('/')
+      .reply(200, mockDocument('Scratched document'));
+
+    nock('http://google.com')
+      .defaultReplyHeaders({
+        'Content-Type': 'text/html',
+      })
+      .get('/')
+      .reply(200, mockDocument('Google template title'));
+
+    nock('http://dummy.com')
+      .defaultReplyHeaders({
+        'Content-Type': 'text/html',
+      })
+      .get('/')
+      .reply(200, mockDummyDocument());
+
+    nock('http://example.com')
+      .defaultReplyHeaders({
+        'Content-Type': 'image/png',
+      })
+      .get('/images/example.png')
+      .reply(200, '');
+
+    db.collections.notes.remove(done);
+  });
+
+  describe('GET /notes', () => {
+    it('Должен возвращаться пустой список заметок', done => {
+      request(application)
+        .get('/api/v1/notes')
+        .expect(200)
+        .then(response => {
+          const { body: { status, result } } = response;
+
+          expect(status).toBe(OK);
+          expect(result).toHaveProperty('notes', []);
+          done();
+        });
+    });
+  });
+
+  describe('GET /keywords', () => {
+    it('Должен вернуться пустой список ключевых слов', done => {
+      request(application)
+        .get('/api/v1/keywords')
+        .expect(200)
+        .then(response => {
+          const { body: { status, result } } = response;
+
+          expect(status).toBe(OK);
+          expect(result).toHaveProperty('keywords', []);
+          done();
+        });
+    });
+
+    it('Должен вернуться список ключевых слов', done => {
+      const text = `This domain is established to be used for illustrative examples in documents.
+        
+        #mlab #development`;
+
+      request(application)
+        .post('/api/v1/notes')
+        .send({ body: text })
+        .expect(200)
+        .then(() => {
+          request(application)
+            .get('/api/v1/keywords')
+            .expect(200)
+            .then(response => {
+              const { body: { status, result } } = response;
+
+              expect(status).toBe(OK);
+              expect(result).toHaveProperty('keywords', ['development', 'mlab']);
+              done();
+            });
+        });
+    });
+
+    it('Должна вернуться заметка по ключевому слову', done => {
+      const text = `This domain is established to be used for illustrative examples in documents.
+        
+        #mlab #development`;
+
+      request(application)
+        .post('/api/v1/notes')
+        .send({ body: text })
+        .expect(200)
+        .then(() => {
+          request(application)
+            .get('/api/v1/keywords/development')
+            .expect(200)
+            .then(response => {
+              const { body: { status, result } } = response;
+              const { notes } = result;
+
+              expect(status).toBe(OK);
+              expect(notes.length).toBe(1);
+              done();
+            });
+        });
+    });
+
+    it('Должна вернуться ошибка отсутствия заметок', done => {
+      request(application)
+        .get('/api/v1/keywords/not-found')
+        .expect(200)
+        .then(response => {
+          const { body: { status } } = response;
+
+          expect(status).toBe(NOT_FOUND);
+          done();
+        });
+    });
+  });
+
+  describe('GET /note/:noteName', () => {
+    it('Должна вернуться ошибка ненайденной заметки', done => {
+      request(application)
+        .get('/api/v1/notes/not-found')
+        .expect(200)
+        .then(response => {
+          const { body: { status } } = response;
+
+          expect(status).toBe(NOT_FOUND);
+          done();
+        });
+    });
+  });
+
+  describe('POST /note', () => {
+    it('Должна быть сохранена и вернуться оформленная заметка', done => {
+      const text = `This domain is established to be used for illustrative examples in documents.
+        You may use this domain in examples without prior coordination or asking for permission.
+        http://example.com/
+        
+        #mlab #development`;
+
+      request(application)
+        .post('/api/v1/notes')
+        .send({ body: text })
+        .expect(200)
+        .then(response => {
+          const { body: { status, result } } = response;
+          const { attachments, keywords } = result;
+
+          expect(status).toBe(OK);
+          expect(result).toHaveProperty('type', 'article');
+          expect(attachments[0].url).toBe('http://example.com');
+          expect(keywords[0].title).toBe('mlab');
+          expect(keywords[1].title).toBe('development');
+          done();
+        });
+    });
+
+    it('Должна вернуться оформленная заметка c вложением сайтом', done => {
+      const text = `This domain is established to be used for illustrative examples in documents.
+        You may use this domain in examples without prior coordination or asking for permission.
+        
+        http://example.com/`;
+
+      request(application)
+        .post('/api/v1/notes')
+        .send({ body: text })
+        .expect(200)
+        .then(response => {
+          const { body } = response;
+
+          expect(body.status).toBe(OK);
+
+          setTimeout(() => {
+            request(application)
+              .get(`/api/v1/notes/${body.result.name}`)
+              .expect(200)
+              .then(response2 => {
+                const { body: { status, result } } = response2;
+                const { attachments } = result;
+
+                expect(status).toBe(OK);
+                expect(attachments[0]).toHaveProperty('title', 'Scratched document');
+                expect(attachments[0]).toHaveProperty('ogTitle', 'OpenGraph Title: Scratched document');
+                expect(attachments[0]).toHaveProperty('ogDescription', 'OpenGraph Description');
+                expect(attachments[0]).toHaveProperty('image', 'http://example.com/images/example.png');
+                done();
+              });
+          }, 1000);
+        });
+    });
+
+    it('Должна вернуться оформленная заметка c вложением картинкой', done => {
+      const text = `This domain is established to be used for illustrative examples in documents.
+        You may use this domain in examples without prior coordination or asking for permission.
+        
+        http://example.com/images/example.png`;
+
+      request(application)
+        .post('/api/v1/notes')
+        .send({ body: text })
+        .expect(200)
+        .then(response => {
+          const { body } = response;
+
+          expect(body.status).toBe(OK);
+
+          setTimeout(() => {
+            request(application)
+              .get(`/api/v1/notes/${body.result.name}`)
+              .expect(200)
+              .then(response2 => {
+                const { body: { status, result } } = response2;
+                const { attachments } = result;
+
+                expect(status).toBe(OK);
+                expect(attachments[0]).toHaveProperty('type', 'image');
+                expect(attachments[0]).toHaveProperty('image', 'http://example.com/images/example.png');
+                done();
+              });
+          }, 1000);
+        });
+    });
+  });
+
+  describe('DELETE /note', () => {
+    it('Должна быть ошибка удаления при неизвестном имени', done => {
+      request(application)
+        .delete('/api/v1/notes/not-found')
+        .expect(200)
+        .then(response2 => {
+          const { body: { status } } = response2;
+
+          expect(status).toBe(NOT_FOUND);
+          done();
+        });
+    });
+
+    it('Должна удалиться заметка по переданному имени', done => {
+      const text = 'This domain is established to be used for illustrative examples in documents.';
+
+      request(application)
+        .post('/api/v1/notes')
+        .send({ body: text })
+        .expect(200)
+        .then(response => {
+          const { body } = response;
+
+          expect(body.status).toBe(OK);
+
+          setTimeout(() => {
+            request(application)
+              .delete(`/api/v1/notes/${body.result.name}`)
+              .expect(200)
+              .then(response2 => {
+                const { body: { status } } = response2;
+
+                expect(status).toBe(OK);
+                done();
+              });
+          }, 1000);
+        });
+    });
+  });
+});
---
+++ tests/parser.test.js
@@ -0,0 +1,104 @@
+const parseNote = require('../application/scripts/parse-note');
+
+let note;
+
+describe('Тестирование парсинга пустой заметки', () => {
+  beforeEach(() => {
+    note = parseNote('');
+  });
+
+  it('Должно быть поле keywords с пустым массивом', () => {
+    expect(note).toHaveProperty('keywords', []);
+  });
+
+  it('Должно быть поле emails с пустым массивом', () => {
+    expect(note).toHaveProperty('emails', []);
+  });
+
+  it('Должно быть поле urls с пустым массивом', () => {
+    expect(note).toHaveProperty('urls', []);
+  });
+});
+
+describe('Проверка парсинга хештегов', () => {
+  it('Должно быть поле keywords с массивом из одного хештега', () => {
+    expect(parseNote('Тестовая заметка #test'))
+      .toHaveProperty('keywords', ['test']);
+  });
+
+  it('Должно быть поле keywords с массивом хештегов', () => {
+    expect(parseNote('Тестовая заметка #test #twitter #foo#bar #brok/en'))
+      .toHaveProperty('keywords', ['test', 'twitter', 'brok']);
+  });
+});
+
+describe('Проверка парсинга емайл адресов', () => {
+  it('Должно быть поле emails с массивом из одного емайла', () => {
+    expect(parseNote('Тестовая заметка email@domain.com'))
+      .toHaveProperty('emails', ['email@domain.com']);
+  });
+
+  it('Должно быть поле emails с массивом емайлов', () => {
+    expect(parseNote('Тестовая заметка email@domain.com broken @domain.com test@test.net'))
+      .toHaveProperty('emails', ['email@domain.com', 'test@test.net']);
+  });
+});
+
+describe('Проверка парсинга ссылок', () => {
+  it('Должно быть парсинг адреса без http', () => {
+    expect(parseNote('Тестовая заметка google.com'))
+      .toHaveProperty('urls', ['http://google.com']);
+  });
+
+  it('Должно быть парсинг адреса c http', () => {
+    expect(parseNote('Тестовая заметка http://amazon.com'))
+      .toHaveProperty('urls', ['http://amazon.com']);
+  });
+
+  it('Должно быть парсинг нескольких адресов', () => {
+    expect(parseNote('Тестовая заметка http://amazon.com google.com'))
+      .toHaveProperty('urls', ['http://amazon.com', 'http://google.com']);
+  });
+
+  it('Должно быть парсинг нескольких адресов разных зон', () => {
+    expect(parseNote('Тестовая заметка amazon.com google.net ya.ru https://vk.com/ вот тут всё'))
+      .toHaveProperty('urls', [
+        'http://amazon.com',
+        'http://google.net',
+        'http://ya.ru',
+        'https://vk.com/',
+      ]);
+  });
+
+  it('Должно быть преобразование длинного адреса vk-wall в короткий', () => {
+    expect(parseNote('Тестовая заметка https://vk.com/feed?w=wall-54530371_143755'))
+      .toHaveProperty('urls', ['https://vk.com/wall-54530371_143755']);
+  });
+
+  it('Должно быть преобразование длинного адреса medium в короткий', () => {
+    expect(parseNote('Тестовая заметка https://medium.com/@ddavisgraphics/programming-isnt-easy-coding-is-a1efcf97a264'))
+      .toHaveProperty('urls', ['https://medium.com/@ddavisgraphics/a1efcf97a264']);
+  });
+
+  it('Должно быть преобразование адресов vk-wall и medium', () => {
+    expect(parseNote(`
+      Тестовая заметка
+      https://medium.com/@ddavisgraphics/programming-isnt-easy-coding-is-a1efcf97a264
+      https://vk.com/feed?w=wall-54530371_143755
+     `))
+      .toHaveProperty('urls', [
+        'https://medium.com/@ddavisgraphics/a1efcf97a264',
+        'https://vk.com/wall-54530371_143755',
+      ]);
+  });
+
+  it('Должно быть парсинг ссылки с путями', () => {
+    expect(parseNote('Тестовая заметка https://regex101.com/r/URKgLt/1'))
+      .toHaveProperty('urls', ['https://regex101.com/r/URKgLt/1']);
+  });
+
+  it('Должно быть парсинг ссылки на изображение', () => {
+    expect(parseNote('Тестовая заметка http://chaijs.com/img/chai-logo-small.png - картинка'))
+      .toHaveProperty('urls', ['http://chaijs.com/img/chai-logo-small.png']);
+  });
+});
