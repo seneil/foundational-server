f5fa016443187d0cc4ebe7be24cc8e08761f360f
dshster <dmitry@shvalyov.ru>
2018-01-08 17:37:13 +0300

    [KPR-42] Adding a note edit.

--- application/router/index.js
+++ application/router/index.js
@@ -12,6 +12,7 @@ const getNotes = require('./routes/get-notes');
 const getNote = require('./routes/get-note');
 const postNote = require('./routes/post-note');
 const putNotes = require('./routes/put-notes');
+const putNote = require('./routes/put-note');
 const deleteNote = require('./routes/delete-note');
 
 const signup = require('./accounts/signup');
@@ -27,6 +28,7 @@ router.route('/v1/notes').get(passport.authenticate('jwt', { session: false }), 
 router.route('/v1/notes/:name').get(passport.authenticate('jwt', { session: false }), getNote);
 router.route('/v1/notes').post(passport.authenticate('jwt', { session: false }), postNote);
 router.route('/v1/notes').put(passport.authenticate('jwt', { session: false }), putNotes);
+router.route('/v1/notes/:name').put(passport.authenticate('jwt', { session: false }), putNote);
 router.route('/v1/notes/:name').delete(passport.authenticate('jwt', { session: false }), deleteNote);
 
 router.route('/v1/profile').get(passport.authenticate('jwt', { session: false }), profile);
---
+++ application/router/routes/put-note/index.js
@@ -0,0 +1,99 @@
+const mongoose = require('mongoose');
+const { OK, NOT_FOUND, NO_VALIDATE } = require('../../constants/answer-codes');
+
+const scrapeUrl = require('../../../scripts/scrape-url');
+const parseNote = require('../../../scripts/parse-note');
+const updateNoteData = require('../../../scripts/update-note-data');
+
+const noteSchema = require('../../../schemas/note.schema');
+const attachmentSchema = require('../../../schemas/attachment.schema');
+
+const Note = mongoose.model('Note', noteSchema);
+const Attachment = mongoose.model('Attachment', attachmentSchema);
+
+const scrapeUrls = (_id, iterator) => {
+  const scrape = () => {
+    const { value, done } = iterator.next();
+
+    if (!done) {
+      const [url] = value;
+
+      Note
+        .findOne({
+          _id,
+          attachments: {
+            $elemMatch: {
+              url: { $eq: url },
+            },
+          },
+        })
+        .then(note => Promise.all([note, scrapeUrl(url)]))
+        .then(([note, scraped]) => Note
+          .updateOne({
+            _id: note._id, 'attachments.url': url,
+          }, {
+            $set: {
+              'attachments.$': new Attachment({ url, ...scraped }),
+            },
+          }))
+        .then(result => {
+          console.log(`Update attachment: ${url}`, { result });
+          scrape();
+        })
+        .catch(error => {
+          console.log({ error });
+          scrape();
+        });
+    }
+  };
+
+  scrape();
+};
+
+module.exports = (request, response) => {
+  const { params: { name }, body: { body }, user: { _id: accountId } } = request;
+
+  if (!body) {
+    return response.status(200)
+      .json({ status: NO_VALIDATE });
+  }
+
+  return Note
+    .findOne({ name, account: accountId })
+    .then(result => {
+      if (result) {
+        const noteData = parseNote(body);
+        const { _id } = result;
+        const { keywords, emails, attachments } = updateNoteData(result, noteData);
+
+        return Note
+          .updateOne({ _id }, {
+            $set: { body, keywords, emails, attachments, updated: new Date() },
+          })
+          .then(updated => {
+            if (attachments.length) {
+              const attachmentsIterator = attachments
+                .filter(attachment => !attachment.title)
+                .reduce((list, attachment) => list.add(attachment.url), new Set())
+                .entries();
+
+              scrapeUrls(_id, attachmentsIterator);
+            }
+
+            return response.status(200)
+              .json({
+                status: OK,
+                result: {
+                  modified: updated.nModified,
+                },
+              });
+          });
+      }
+
+      return Promise.reject(NOT_FOUND);
+    })
+    .catch(error => {
+      response.status(200)
+        .json({ status: error.errors || error });
+    });
+};
