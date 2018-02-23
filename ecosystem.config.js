fd0ae5880943834cb124638583c15215b43d38aa
dshster <dmitry@shvalyov.ru>
2018-02-23 21:53:49 +0300

    [KPR-51] Configuring PM2 settings.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,11 +2,20 @@ module.exports = {
   apps: [{
     name: 'application',
     script: './application/index.js',
-    ignore_watch: ['.git', 'node_modules', 'mdb', 'assets'],
+    ignore_watch: ['.git', 'node_modules', 'mdb', 'assets', 'storage', 'coverage'],
     max_memory_restart: '150M',
     max_restarts: 10,
     env: {
       NODE_ENV: 'development',
+      NODE_APP_INSTANCE: '',
+    },
+    env_staging: {
+      NODE_ENV: 'development',
+      NODE_APP_INSTANCE: 'staging',
+    },
+    env_production: {
+      NODE_ENV: 'deployment',
+      NODE_APP_INSTANCE: 'deployment',
     },
   }],
   deploy: {
@@ -18,7 +27,7 @@ module.exports = {
       path: '/var/www/api.keeper.shvalyov.ru',
       'pre-deploy': 'git fetch --all',
       'post-setup': 'ls -la',
-      'post-deploy': 'npm install --production && pm2 startOrRestart ecosystem.config.js',
+      'post-deploy': 'npm install --production && pm2 startOrRestart ecosystem.config.js --env staging',
     },
     production: {
       user: 'dshster',
@@ -27,7 +36,7 @@ module.exports = {
       repo: 'https://github.com/seneil/note-keeper-server.git',
       path: '/var/www/api.keeper.shvalyov.ru',
       'post-setup': 'ls -la',
-      'post-deploy': 'npm install --production && pm2 startOrRestart ecosystem.config.js',
+      'post-deploy': 'npm install --production && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
 };
